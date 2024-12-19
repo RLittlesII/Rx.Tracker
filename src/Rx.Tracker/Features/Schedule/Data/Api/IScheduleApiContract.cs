1ecb57b5a06d4cacf92bdf1b4349c4a73092e5e1
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2024-12-19 01:20:04 -0600

    add query parameters to api contract

--- src/Rx.Tracker/Features/Schedule/Data/Api/IScheduleApiContract.cs
+++ src/Rx.Tracker/Features/Schedule/Data/Api/IScheduleApiContract.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 using Refit;
 using Rx.Tracker.Features.Schedule.Data.Dto;
+using Rx.Tracker.Features.Schedule.Domain.Queries;
 
 namespace Rx.Tracker.Features.Schedule.Data.Api;
 
@@ -13,7 +14,8 @@ public interface IScheduleApiContract
     /// Get a set of <see cref="ScheduleDto"/>.
     /// </summary>
     /// <param name="userId">The userId.</param>
+    /// <param name="query">The query.</param>
     /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
     [Get("/schedule/{id}")]
-    Task<ScheduleDto> Get([AliasAs("id")] string userId);
+    Task<ScheduleDto> Get([AliasAs("id")] string userId, [Query] LoadSchedule.Query query);
 }
