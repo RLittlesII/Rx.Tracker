a852a77d4e6f41871b0305178abc85577cce5025
Rodney Littles II <6969701+RLittlesII@users.noreply.github.com>
2025-01-03 22:27:24 -0600

    feature: add ScheduleViewModel (#7)
    * feature: add ScheduleViewModel
    
    View Model
    State Machine
    MedicationSchedule
    
    * UtcNow
    
    * tests passing
    
    better names?  worst?

---
+++ test/Rx.Tracker.Tests/Features/Schedule/ViewModels/ScheduleStateMachineFixture.cs
@@ -0,0 +1,7 @@
+using Rocket.Surgery.Extensions.Testing.AutoFixtures;
+using Rx.Tracker.Features.Schedule.ViewModels;
+
+namespace Rx.Tracker.Tests.Features.Schedule.ViewModels;
+
+[AutoFixture(typeof(ScheduleStateMachine))]
+internal partial class ScheduleStateMachineFixture;
---
+++ test/Rx.Tracker.Tests/Features/Schedule/ViewModels/ScheduleViewModelTests.cs
@@ -0,0 +1,134 @@
+using FluentAssertions;
+using NSubstitute;
+using Rx.Tracker.Features.Schedule.Domain.Entities;
+using Rx.Tracker.Features.Schedule.Domain.Queries;
+using Rx.Tracker.Features.Schedule.ViewModels;
+using Rx.Tracker.Mediation;
+using Rx.Tracker.Tests.Features.Medicine.Domain.Entities;
+using Rx.Tracker.Tests.Features.Schedule.Domain.Entities;
+using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+
+namespace Rx.Tracker.Tests.Features.Schedule.ViewModels;
+
+public class ScheduleViewModelTests
+{
+    [Fact]
+    public void WhenConstructed_ThenCurrentStateShouldBeInitial()
+    {
+        // Given
+        ScheduleViewModel sut = new ScheduleViewModelFixture();
+
+        // When, Then
+        sut.CurrentState.Should().Be(ScheduleStateMachine.ScheduleState.Initial);
+    }
+
+    [Fact]
+    public async Task GivenNoResult_WhenInitialized_ThenCurrentStateShouldBeBusy()
+    {
+        // Given
+        ScheduleViewModel sut = new ScheduleViewModelFixture();
+
+        // When
+        await sut.InitializeCommand.Execute(Unit.Default);
+
+        // Then
+        sut.CurrentState.Should().Be(ScheduleStateMachine.ScheduleState.Busy);
+    }
+
+    [Fact]
+    public async Task GivenLoadScheduleResult_WhenInitialized_ThenCurrentStateShouldBeLoaded()
+    {
+        // Given
+        var cqrs = Substitute.For<ICqrs>();
+        cqrs.Query(Arg.Any<LoadSchedule.Query>()).Returns(Task.FromResult(new LoadSchedule.Result(new MedicationScheduleFixture().WithEnumerable([]))));
+        ScheduleViewModel sut = new ScheduleViewModelFixture().WithCqrs(cqrs);
+
+        // When
+        await sut.InitializeCommand.Execute(Unit.Default);
+
+        // Then
+        sut.CurrentState.Should().Be(ScheduleStateMachine.ScheduleState.Loaded);
+    }
+
+    [Fact]
+    public async Task GivenLoadScheduleResult_WhenInitialized_ThenWeekShouldNotBeNull()
+    {
+        // Given
+        var cqrs = Substitute.For<ICqrs>();
+        cqrs.Query(Arg.Any<LoadSchedule.Query>()).Returns(
+            Task.FromResult(
+                new LoadSchedule.Result(
+                    new MedicationScheduleFixture().WithEnumerable(
+                        [
+                            new ScheduledMedicationFixture().WithScheduledTime(DateTimeOffset.UtcNow),
+                        ]
+                    )
+                )
+            )
+        );
+        ScheduleViewModel sut = new ScheduleViewModelFixture().WithCqrs(cqrs);
+
+        // When
+        await sut.InitializeCommand.Execute(Unit.Default);
+
+        // Then
+        sut.Week.Should().NotBeNullOrEmpty();
+    }
+
+    [Fact]
+    public async Task GivenLoadScheduleResult_WhenInitialized_ThenScheduleShouldNotBeNull()
+    {
+        // Given
+        var cqrs = Substitute.For<ICqrs>();
+        cqrs.Query(Arg.Any<LoadSchedule.Query>()).Returns(
+            Task.FromResult(
+                new LoadSchedule.Result(
+                    new MedicationScheduleFixture().WithEnumerable(
+                        [
+                            new ScheduledMedicationFixture().WithScheduledTime(DateTimeOffset.UtcNow),
+                        ]
+                    )
+                )
+            )
+        );
+        ScheduleViewModel sut = new ScheduleViewModelFixture().WithCqrs(cqrs);
+
+        // When
+        await sut.InitializeCommand.Execute(Unit.Default);
+
+        // Then
+        sut.Schedule.Should().NotBeNullOrEmpty();
+    }
+
+    [Fact]
+    public async Task GivenLoadScheduleResult_WhenInitialized_ThenScheduleShouldBeForDate()
+    {
+        // Given
+        var now = DateTimeOffset.UtcNow;
+        var cqrs = Substitute.For<ICqrs>();
+        cqrs.Query(Arg.Any<LoadSchedule.Query>()).Returns(
+            Task.FromResult(
+                new LoadSchedule.Result(
+                    new MedicationScheduleFixture().WithEnumerable(
+                        [
+                            new ScheduledMedicationFixture().WithScheduledTime(now),
+                            new ScheduledMedicationFixture().WithScheduledTime(now),
+                            new ScheduledMedicationFixture().WithScheduledTime(now),
+                            new ScheduledMedicationFixture().WithScheduledTime(now.AddDays(2)),
+                        ]
+                    )
+                )
+            )
+        );
+        ScheduleViewModel sut = new ScheduleViewModelFixture().WithCqrs(cqrs);
+
+        // When
+        await sut.InitializeCommand.Execute(Unit.Default);
+
+        // Then
+        sut.Schedule.Should().HaveCount(3).And.Subject.Should().OnlyContain(scheduledMedication => scheduledMedication.ScheduledTime == now);
+    }
+}
