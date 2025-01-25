055dd4fc350eb72bb0c8e24414ee9281f7777907
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2025-01-25 15:10:06 -0600

    chore: implement IDestructible

--- src/UI/Features/Components/ScreenBase.cs
+++ src/UI/Features/Components/ScreenBase.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Reactive;
+using System.Reactive.Disposables;
 using System.Threading.Tasks;
 using Prism.Navigation;
 using ReactiveUI.Maui;
@@ -13,15 +14,20 @@ namespace Rx.Tracker.UI.Features.Components;
 /// Represents a base screen with implementations for lifecycle methods.
 /// </summary>
 /// <typeparam name="T">The View Model Type.</typeparam>
-public abstract class ScreenBase<T> : ReactiveContentPage<T>, IInitializeAsync, INavigatedAware
+public abstract class ScreenBase<T> : ReactiveContentPage<T>, IInitializeAsync, INavigatedAware, IDestructible
     where T : ViewModelBase
 {
     /// <inheritdoc/>
     Task IInitializeAsync.InitializeAsync(INavigationParameters parameters) => InitializeAsync(parameters);
 
+    /// <inheritdoc />
     void INavigatedAware.OnNavigatedFrom(INavigationParameters parameters) => OnNavigatedFrom(parameters);
 
+    /// <inheritdoc />
     void INavigatedAware.OnNavigatedTo(INavigationParameters parameters) => OnNavigatedTo(parameters);
+
+    /// <inheritdoc />
+    void IDestructible.Destroy() => TearDown();
 
     /// <summary>
     /// Initialize the instance.
@@ -30,11 +36,20 @@ public abstract class ScreenBase<T> : ReactiveContentPage<T>, IInitializeAsync, 
     /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
     protected virtual Task Initialize(INavigationParameters parameters) => Task.CompletedTask;
 
+    /// <summary>
+    /// Gets the garbage for disposal.
+    /// </summary>
+    protected CompositeDisposable Garbage { get; } = new();
+
+    protected virtual void Destroy()
+    {
+    }
+
     private async Task InitializeAsync(INavigationParameters parameters)
     {
         await Initialize(parameters);
 
-        using var disposable = ViewModel?.InitializeCommand.Execute(Unit.Default).Subscribe();
+        using var disposable = ViewModel?.DisposeWith(Garbage).InitializeCommand.Execute(Unit.Default).Subscribe();
     }
 
     private void OnNavigatedFrom(INavigationParameters parameters)
@@ -49,5 +64,11 @@ public abstract class ScreenBase<T> : ReactiveContentPage<T>, IInitializeAsync, 
         INavigated? navigatedTo = ViewModel;
 
         navigatedTo?.OnNavigatedTo(new NavigatorArguments(parameters));
+    }
+
+    private void TearDown()
+    {
+        Garbage.Dispose();
+        Destroy();
     }
 }
