5758130fd485c282f001038eaf95c6da02bbd54c
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2025-01-26 22:45:36 -0600

    fix color

--- src/UI/Features/Schedule/CalendarStorageFacade.cs
+++ src/UI/Features/Schedule/CalendarStorageFacade.cs
@@ -1,7 +1,6 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Microsoft.Maui.Graphics;
-using NodaTime;
 using Plugin.Maui.CalendarStore;
 using Rx.Tracker.Features.Schedule.Data.Dto;
 using Rx.Tracker.Features.Schedule.Data.Store;
@@ -13,61 +12,45 @@ public class CalendarStorageFacade : ICalendarStorage, ICalendarEventStorage
 {
     public CalendarStorageFacade(ICalendarStore calendarStore) => _calendarStore = calendarStore;
 
-    Task<CalendarId> ICalendarStorage.Create(CalendarDto calendar) => _calendarStore.CreateCalendar(calendar.Name, Color.FromInt(calendar.Color)).ContinueWith(task => new CalendarId(task.Result));
+    Task<CalendarId> ICalendarStorage.Create(string name, int color) => throw new NotImplementedException();
 
     Task<CalendarEventId> ICalendarEventStorage.Create(
         CalendarId calendarId,
         string title,
         string description,
         string location,
-        OffsetDateTime startDateTime,
-        OffsetDateTime endDateTime,
+        DateTimeOffset startDateTime,
+        DateTimeOffset endDateTime,
         bool isAllDay,
-        ReminderDto[]? reminders) => _calendarStore.CreateEvent(
-        calendarId.ToString(),
-        title,
-        description,
-        location,
-        startDateTime.ToDateTimeOffset(),
-        endDateTime.ToDateTimeOffset(),
-        isAllDay).ContinueWith(task => new CalendarEventId(task.Result));
+        ReminderDto[]? reminders) => throw new NotImplementedException();
 
-    Task<IReadOnlyList<CalendarEventDto>> ICalendarEventStorage.Read(CalendarId calendarId) => throw new System.NotImplementedException();
+    Task<CalendarEventId> ICalendarEventStorage.Create(CalendarEventDto calendarEvent) => throw new NotImplementedException();
 
-    Task<CalendarEventDto> ICalendarEventStorage.Read(CalendarEventId eventId) => throw new System.NotImplementedException();
+    Task<IEnumerable<CalendarEventDto>> ICalendarEventStorage.Read(CalendarId calendarId) => throw new NotImplementedException();
 
-    Task<CalendarEventId> ICalendarEventStorage.Create(CalendarEventDto calendarEvent) => throw new System.NotImplementedException();
-
-    Task ICalendarEventStorage.UpdateEvent(
-        CalendarEventId eventId,
+    Task ICalendarEventStorage.Update(
+        CalendarEventId id,
         string title,
         string description,
         string location,
-        OffsetDateTime startDateTime,
-        OffsetDateTime endDateTime,
+        DateTimeOffset startDateTime,
+        DateTimeOffset endDateTime,
         bool isAllDay,
-        ReminderDto[]? reminders) => _calendarStore.UpdateEvent(
-        eventId.ToString(),
-        title,
-        description,
-        location,
-        startDateTime.ToDateTimeOffset(),
-        endDateTime.ToDateTimeOffset(),
-        isAllDay);
+        ReminderDto[]? reminders) => throw new NotImplementedException();
 
-    Task ICalendarEventStorage.UpdateEvent(CalendarEventDto eventToUpdate) => throw new System.NotImplementedException();
+    Task ICalendarEventStorage.Update(CalendarEventDto @event) => throw new NotImplementedException();
 
-    Task ICalendarEventStorage.DeleteEvent(CalendarEventId eventId) => throw new System.NotImplementedException();
+    Task ICalendarEventStorage.Delete(CalendarEventId id) => throw new NotImplementedException();
 
-    Task ICalendarEventStorage.DeleteEvent(CalendarEventDto eventToDelete) => throw new System.NotImplementedException();
+    Task ICalendarEventStorage.Delete(CalendarEventDto @event) => throw new NotImplementedException();
 
-    Task<CalendarDto> ICalendarStorage.Read(CalendarId calendarId) => throw new System.NotImplementedException();
+    Task<CalendarDto> ICalendarStorage.Read(CalendarId calendarId) => throw new NotImplementedException();
 
-    Task ICalendarStorage.Update(CalendarDto calendar) => throw new System.NotImplementedException();
+    Task ICalendarStorage.Update(CalendarId id, string name, int color) => throw new NotImplementedException();
 
-    Task ICalendarStorage.Delete(CalendarId calendarId) => throw new System.NotImplementedException();
+    Task ICalendarStorage.Delete(CalendarId calendarId) => throw new NotImplementedException();
 
-    Task ICalendarStorage.Delete(CalendarDto calendarToDelete) => throw new System.NotImplementedException();
+    Task ICalendarStorage.Delete(CalendarDto delete) => throw new NotImplementedException();
 
     private readonly ICalendarStore _calendarStore;
 }
