0057c8ebf516622482937977f47aa456e78ac92b
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2025-02-02 18:50:11 -0600

    split out schedule view model state tests

--- test/Rx.Tracker.Tests/Features/Schedule/ViewModels/ScheduleViewModelTests.cs
+++ test/Rx.Tracker.Tests/Features/Schedule/ViewModels/ScheduleViewModelTests.cs
@@ -17,44 +17,6 @@ namespace Rx.Tracker.Tests.Features.Schedule.ViewModels;
 public partial class ScheduleViewModelTests
 {
     [Fact]
-    public void WhenConstructed_ThenCurrentStateShouldBeInitial()
-    {
-        // Given
-        ScheduleViewModel sut = new ScheduleViewModelFixture();
-
-        // When, Then
-        sut.CurrentState.Should().Be(ScheduleStateMachine.ScheduleState.Initial);
-    }
-
-    [Fact]
-    public async Task GivenNoResult_WhenInitialized_ThenCurrentStateShouldBeFailed()
-    {
-        // Given
-        ScheduleViewModel sut = new ScheduleViewModelFixture();
-
-        // When
-        await sut.InitializeCommand.Execute(Unit.Default);
-
-        // Then
-        sut.CurrentState.Should().Be(ScheduleStateMachine.ScheduleState.Failed);
-    }
-
-    [Fact]
-    public async Task GivenLoadScheduleResult_WhenInitialized_ThenCurrentStateShouldBeDaySchedule()
-    {
-        // Given
-        var cqrs = Substitute.For<ICqrs>();
-        cqrs.Query(Arg.Any<LoadSchedule.Query>()).Returns(Task.FromResult(new LoadSchedule.Result(new MedicationScheduleFixture().WithEnumerable([]))));
-        ScheduleViewModel sut = new ScheduleViewModelFixture().WithCqrs(cqrs);
-
-        // When
-        await sut.InitializeCommand.Execute(Unit.Default);
-
-        // Then
-        sut.CurrentState.Should().Be(ScheduleStateMachine.ScheduleState.DaySchedule);
-    }
-
-    [Fact]
     public async Task GivenLoadScheduleResult_WhenInitialized_ThenScheduleShouldNotBeNull()
     {
         // Given
