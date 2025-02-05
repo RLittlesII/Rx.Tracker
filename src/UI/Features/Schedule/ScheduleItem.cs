66881fd35145dda82bf97973c443e750148d3e2b
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2025-02-05 09:29:55 +0100

    figure out schedule item

--- src/UI/Features/Schedule/ScheduleItem.cs
+++ src/UI/Features/Schedule/ScheduleItem.cs
@@ -1,4 +1,5 @@
 using CommunityToolkit.Maui.Markup;
+using Microsoft.Maui;
 using Microsoft.Maui.Controls;
 using Rx.Tracker.Features.Schedule.Domain.Entities;
 
@@ -6,12 +7,44 @@ namespace Rx.Tracker.UI.Features.Schedule;
 
 public class ScheduleItem : ViewCell
 {
-    public ScheduleItem() => View = new StackLayout
+    public ScheduleItem() => View = new Grid
     {
+        Margin = new Thickness(12, 0),
+        ColumnDefinitions =
+            GridRowsColumns.Columns.Define(
+                (Column.Name, GridLength.Auto),
+                (Column.Space, GridLength.Star),
+                (Column.Time, GridLength.Auto)),
         Children =
         {
             new Label()
-            .Bind(Label.TextProperty, (ScheduledMedication medication) => medication.Medication.Id)
+               .Start()
+               .Column(Column.Name)
+               .Bind(Label.TextProperty, (ScheduledMedication medication) => medication.Medication.Id),
+            new Label()
+               .Column(Column.Time)
+               .Bind(
+                    Label.TextProperty,
+                    (ScheduledMedication medication) => medication.ScheduledTime,
+                    convert: time => time.TimeOfDay.ToTimeOnly().ToShortTimeString()),
         }
     };
+
+    private enum Column
+    {
+        /// <summary>
+        /// The name.
+        /// </summary>
+        Name,
+
+        /// <summary>
+        /// The final frontier.
+        /// </summary>
+        Space,
+
+        /// <summary>
+        /// The time.
+        /// </summary>
+        Time
+    }
 }
